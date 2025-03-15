c9f502676390f7fc58db98c7ac4643d92863995a
FlyLonyx <147259673+FlyLonyx@users.noreply.github.com>
2025-03-15 12:42:54 +0100

    update - 1.0.24

--- src/assets/js/panels/home.js
+++ src/assets/js/panels/home.js
@@ -108,7 +108,7 @@ class Home {
         let opts = {
           url:
             this.config.game_url === "" || this.config.game_url === undefined
-              ? `${urlpkg}/filessss`
+              ? `${urlpkg}/filesssss`
               : this.config.game_url,
           authenticator: account,
           timeout: 10000,
